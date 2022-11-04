79911e3b71bc0085da56bb248825ebd24378c1c1
matyii <kmatyas2004@gmail.com>
2022-11-04 16:37:58 +0100

    views instead of html sends

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const filesizejs = require('filesize')
 const fs = require('fs')
 const path = require('path')
 const app = express()
+app.set('view engine', 'ejs');
 const getSomeCoolEmojis = require("get-some-cool-emojis")
 
 var appDir = path.dirname(require.main.filename).toString().replace("src", "")
@@ -21,17 +22,21 @@ var mainDomain = config["maindomain"]
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get("/", (req,res) => {
+    res.render('index')
+})
+
 app.get("/config", (req, res) => {
-    res.sendFile(appDir + "/public/config.html")
+    res.render('config')
 })
 
 app.get("/:file", (req, res) => {
     var file = req.params["file"]
     
-    fs.readdirSync(__dirname + '/raw/').forEach( function (item, index) {
+    fs.readdirSync(__dirname + '/raw/i/').forEach( function (item, index) {
         if (file == item.replace("." + item.split(".")[1], "")) {
-            var filePath = 'raw/' + item
-            var fileUrl = "https://"+mainDomain+"/raw/" + item
+            var filePath = 'raw/i/' + item
+            var fileUrl = "http://"+mainDomain+"/raw/i/" + item
             var fileSize = filesizejs(fs.statSync(__dirname + "/" + filePath).size, {base: 10})
             var extension = item.split(".")[1]
 
@@ -44,20 +49,20 @@ app.get("/:file", (req, res) => {
             var embedColour = uploads[item]["embed"]["colour"]
 
             if (extension == "webm" || extension == "mp4" || extension == "mov") {
-                res.send(`<!DOCTYPE html> <html lang="en"> <head> <title>hypixel.lol — ${file}</title> <link type="application/json+oembed" href="${oEmbed}"> <meta name="twitter:card" content="player"> <meta name="twitter:player" content="${fileUrl}"> <meta name="twitter:player:width" content="1280"> <meta name="twitter:player:height" content="720"> <meta name="twitter:title" content="${embedTitle}"> <meta name="twitter:description" content="${embedDescription}"> <meta name="theme-color" content="${embedColour}"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <link rel="stylesheet" href="assets/style.css"> <link rel="stylesheet" href="https://use.typekit.net/obf6rgz.css"> </head> <body> <div class="center"> <div class="card"> <div class="card-content"> <h2 class="card-title">${item} (${fileSize})</h2> <video style="max-width: 100%; height: auto; border-radius: 10px;" controls> <source src="${filePath}"> </video> </div></div></div></body> </html>`)
+                res.render('video', {item:item,file:file,fileSize:fileSize, oEmbed:oEmbed, fileUrl:fileUrl, embedTitle:embedTitle, embedDescription:embedDescription, embedColour:embedColour, filePath:filePath, user:user})
             } else {
-                res.send(`<!DOCTYPE html> <html lang="en"> <head> <title>hypixel.lol — ${file}</title> <link type="application/json+oembed" href="${oEmbed}"> <meta name="twitter:card" content="photo"> <meta name="twitter:image" content="${fileUrl}"> <meta name="twitter:title" content="${embedTitle}"> <meta name="twitter:description" content="${embedDescription}"> <meta name="theme-color" content="${embedColour}"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <link rel="stylesheet" href="assets/style.css"> <link rel="stylesheet" href="https://use.typekit.net/obf6rgz.css"> </head> <body> <div class="center"> <div class="card"> <div class="card-content"> <h2 class="card-title">${item} (${fileSize})</h2> <img src="${filePath}" style="max-width: 100%; height: auto; border-radius: 10px;"> </div></div></div></body> </html> `)                        
+                res.render('photo', {item:item,file:file,fileSize:fileSize, oEmbed:oEmbed, fileUrl:fileUrl, embedTitle:embedTitle, embedDescription:embedDescription, embedColour:embedColour, filePath:filePath, user:user})                        
             }
         }
     })
 })
 
-app.get("/raw/:file", (req, res) => {
+app.get("/raw/i/:file", (req, res) => {
     var file = req.params["file"]
     
-    fs.readdirSync(__dirname + '/raw/').forEach( function (item, index) {
+    fs.readdirSync(__dirname + '/raw/i/').forEach( function (item, index) {
         if (file == item) {
-            var filePath = __dirname + '/raw/' + item
+            var filePath = __dirname + '/raw/i/' + item
             res.sendFile(filePath)
         }
     })
@@ -91,10 +96,10 @@ app.post("/upload", (req, res) => {
 
         if (uploadKeys.includes(uploadKey)) {
             if (allowedExtensions.includes(extension)) {
-                fs.rename(files.file.path, __dirname + '/raw/' + hash + "." + extension, function (err) {
+                fs.rename(files.file.path, __dirname + '/raw/i/' + hash + "." + extension, function (err) {
                     if (err) throw err
 
-                    fs.writeFileSync(__dirname + "/raw/" + hash + "-embed.json", `{"version":"1.0","type":"link","author_name":"${embedAuthor}"}`)
+                    fs.writeFileSync(__dirname + "/raw/json/" + hash + "-embed.json", `{"version":"1.0","type":"link","author_name":"${embedAuthor}"}`)
 
                     fs.readFile(__dirname + '/data/uploads.json', function (error, data) {
                         if (error) throw error
@@ -102,8 +107,8 @@ app.post("/upload", (req, res) => {
                 
                         uploads[`${hash}.${extension}`] = {}
                         uploads[`${hash}.${extension}`]["user"] = user
-                        uploads[`${hash}.${extension}`]["url"] = `https://${mainDomain}/${hash}`
-                        uploads[`${hash}.${extension}`]["oembed"] = `https://${mainDomain}/raw/${hash}-embed.json`
+                        uploads[`${hash}.${extension}`]["url"] = `http://${mainDomain}/${hash}`
+                        uploads[`${hash}.${extension}`]["oembed"] = `http://${mainDomain}/raw/${hash}-embed.json`
                         uploads[`${hash}.${extension}`]["embed"] = {}
                         uploads[`${hash}.${extension}`]["embed"]["title"] = embedTitle
                         uploads[`${hash}.${extension}`]["embed"]["description"] = embedDescription
@@ -114,15 +119,15 @@ app.post("/upload", (req, res) => {
 
                     if (domains.includes(fields["domain"])) {
                         if (subdomain != "") {
-                            res.write(`https://${subdomain}.${fields["domain"]}/${hash}`)
+                            res.write(`http://${subdomain}.${fields["domain"]}/${hash}`)
                         } else {
-                            res.write(`https://${fields["domain"]}/${hash}`)
+                            res.write(`http://${fields["domain"]}/${hash}`)
                         }
                     } else {
                         if (subdomain != "") {
-                            res.write(`https://${subdomain}.${mainDomain}/${hash}`)
+                            res.write(`http://${subdomain}.${mainDomain}/${hash}`)
                         } else {
-                            res.write(`https://${mainDomain}/${hash}`)
+                            res.write(`http://${mainDomain}/${hash}`)
                         }
                     }
                     res.end()
@@ -159,5 +164,5 @@ app.listen(config["nodeserverport"], () => {
     console.log("listening on :"+config["nodeserverport"])
 })
 
-app.use(express.static(appDir + "/public/"))
+app.use(express.static(appDir + "/views/"))
 app.use('assets/', serveIndex(appDir + '/assets/'))
